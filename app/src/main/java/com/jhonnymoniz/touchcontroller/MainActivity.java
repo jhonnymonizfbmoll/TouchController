49d96808db9ca8d05ef542a45639897f912229cc
Jhonny <jmoniz@cifpfbmoll.eu>
2020-10-29 10:52:54 +0100

    Final commit

--- app/src/main/java/com/jhonnymoniz/touchcontroller/MainActivity.java
+++ app/src/main/java/com/jhonnymoniz/touchcontroller/MainActivity.java
@@ -3,12 +3,13 @@ package com.jhonnymoniz.touchcontroller;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.MotionEventCompat;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private static final String DEBUG_TAG = "MainActivity";
 
@@ -26,15 +27,19 @@ public class MainActivity extends AppCompatActivity {
 
         switch (action) {
             case (MotionEvent.ACTION_DOWN):
+                Toast.makeText(getApplicationContext(),"Action was DOWN", Toast.LENGTH_SHORT).show();
                 Log.d(DEBUG_TAG, "Action was DOWN");
                 return true;
             case (MotionEvent.ACTION_MOVE):
+
                 Log.d(DEBUG_TAG, "Action was MOVE");
                 return true;
             case (MotionEvent.ACTION_UP):
+                Toast.makeText(getApplicationContext(),"Action was UP", Toast.LENGTH_SHORT).show();
                 Log.d(DEBUG_TAG, "Action was UP");
                 return true;
             case (MotionEvent.ACTION_CANCEL):
+                Toast.makeText(getApplicationContext(),"Action was Cancel", Toast.LENGTH_SHORT).show();
                 Log.d(DEBUG_TAG, "Action was CANCEL");
                 return true;
             case (MotionEvent.ACTION_OUTSIDE):
